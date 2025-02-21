0f9e1d65c7dbf4d69467ac2006d1dcd1b6d7226a
rumman321 <cset-210652@dti.ac>
2025-02-21 22:28:40 +0600

    add task design complete using formData and use date-fns

--- src/Pages/ADDTask.jsx
+++ src/Pages/ADDTask.jsx
@@ -1,13 +1,50 @@
-
-
-import React from 'react';
+import { compareAsc, format } from "date-fns";
 
 const ADDTask = () => {
-    return (
-        <div>
+  const time = format(new Date(), "yyyy-MM-dd");
+  
+  const handleAdd = (e)=>{
+    e.preventDefault();
+
+    const form = new FormData(e.target);
+    const name = form.get("title");
+    const email = form.get("message");
+    console.log(email,name);
+  }
+  return (
+    <div>
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto">
+        <form className="card-body" onSubmit={handleAdd}>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">TITLE</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Title"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Description</span>
+            </label>
+            <textarea
+              placeholder="Type Message"
+              name="message"
+              required
+              className="textarea textarea-bordered w-full mb-4"
+            ></textarea>
             
-        </div>
-    );
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">ADD</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ADDTask;
